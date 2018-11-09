b562d0ff3fd63f5c64b12b4b9ae3cce6f4896152
Sergey Ermakovich <s.ermakovich@gmail.com>
2018-11-09 15:46:41 +0300

    upgrade to gatsby v2 final

--- src/components/progressive-image.js
+++ src/components/progressive-image.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import Img from 'gatsby-image'
 
 export default styled(Img)`
-  ${'' /* applying blur to preview in gatsby-image */} img:first-of-type {
+  ${'' /* applying blur to preview in gatsby-image */} img[src^=data] {
     filter: blur(0.2em);
   }
 `
