474ae4991e1db4d52128e5dce89c92d95160e90a
Sergey Ermakovich <s.ermakovich@gmail.com>
2020-11-10 17:20:48 +0300

    add post image previews on posts page

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -6,9 +6,10 @@ import { Helmet } from 'react-helmet'
 import Content from 'components/content'
 import PostMeta from 'components/posts/post-meta'
 import PostDate from 'components/posts/post-date'
+import Img from 'components/progressive-image'
 
 export default function BlogIndex() {
-  const { site, allMarkdownRemark } = useStaticQuery(graphql`
+  const { site, allMarkdownRemark, memoIcon } = useStaticQuery(graphql`
     query {
       site {
         siteMetadata {
@@ -26,7 +27,32 @@ export default function BlogIndex() {
             frontmatter {
               date(formatString: "MMMM DD, YYYY")
               title
+              cover_image {
+                childImageSharp {
+                  fixed(width: 70, height: 70) {
+                    ...GatsbyImageSharpFixed
+                    ...GatsbyImageSharpFixed_noBase64
+                  }
+                }
+                publicURL
+              }
+              image {
+                childImageSharp {
+                  fixed(width: 70, height: 70) {
+                    ...GatsbyImageSharpFixed
+                    ...GatsbyImageSharpFixed_noBase64
+                  }
+                }
+                publicURL
+              }
             }
+          }
+        }
+      }
+      memoIcon: file(relativePath: { eq: "pages/posts/memo.png" }) {
+        childImageSharp {
+          fixed(width: 70, height: 70) {
+            ...GatsbyImageSharpFixed
           }
         }
       }
@@ -41,8 +67,29 @@ export default function BlogIndex() {
       <Content>
         {posts.map(({ node }) => {
           const title = get(node, 'frontmatter.title') || node.fields.slug
+          const { frontmatter } = node
+          const image = frontmatter.image || frontmatter.cover_image
           return (
             <div key={node.fields.slug}>
+              <Link to={node.fields.slug}>
+                <Img
+                  fixed={
+                    image
+                      ? image.childImageSharp.fixed
+                      : memoIcon.childImageSharp.fixed
+                  }
+                  alt="post image"
+                  style={{
+                    width: 70,
+                    height: 70,
+                    flex: 'none',
+                    marginTop: 5,
+                    marginRight: 20,
+                    float: 'left',
+                    borderRadius: 5,
+                  }}
+                />
+              </Link>
               <h3>
                 <Link to={node.fields.slug}>{title}</Link>
               </h3>
