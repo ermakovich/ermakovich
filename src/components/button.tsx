46d27002b811fd43ebe805c8750197784f649eca
Sergey Ermakovich <s.ermakovich@gmail.com>
2024-10-04 20:31:11 +0300

    исправить --color-primary, улучшить окантовку для кнопок

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,7 +5,7 @@ export default styled.button.attrs((props) => ({
 }))`
   background: rgba(0, 0, 0, 0.1);
   color: var(--color-primary);
-  border: 0.1em solid var(--color-primary);
+  border: 1px solid rgba(var(--color-primary-rgb), 0.3);
   margin: 10px;
   padding: 0.7em 2em;
   text-align: center;
@@ -15,6 +15,5 @@ export default styled.button.attrs((props) => ({
 
   @media (prefers-color-scheme: dark) {
     background: rgba(255, 255, 255, 0.1);
-    /* border: 2px solid var(--color-primary); */
   }
 `
