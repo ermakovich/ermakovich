9cde5c8b631e94e227dba334cd80d97e76215bad
Sergey Ermakovich <s.ermakovich@gmail.com>
2024-11-29 13:13:05 +0300

    meta image для страницы проектов

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import styled from 'styled-components'
 
@@ -8,15 +8,21 @@ import ProjectPreview from 'components/projects/project-preview/project-preview'
 
 import { SEO } from 'components/seo'
 import UnstyledList from 'components/unstyled-list'
+import { getSrc } from 'gatsby-plugin-image'
 
-export const Head = () => (
-  <SEO
-    titleAddendum="Проекты"
-    description="Проекты, в которых я принимал участие в качестве веб-разработчика"
-  >
-    <meta name="robots" content="noindex" />
-  </SEO>
-)
+export const Head = ({ data }) => {
+  const projects = data.last.edges
+
+  return (
+    <SEO
+      titleAddendum="Проекты"
+      description="Проекты, в которых я принимал участие в качестве веб-разработчика"
+      image={getSrc(projects[0].node.frontmatter.cover_image)}
+    >
+      <meta name="robots" content="noindex" />
+    </SEO>
+  )
+}
 
 const ListItem = styled.li`
   & + & {
@@ -24,54 +30,8 @@ const ListItem = styled.li`
   }
 `
 
-export default function ProjectsIndex() {
-  const { allMarkdownRemark } = useStaticQuery(graphql`
-    {
-      allMarkdownRemark(
-        filter: { fields: { slug: { regex: "/projects/" } } }
-        sort: { frontmatter: { date: DESC } }
-      ) {
-        edges {
-          node {
-            excerpt
-            fields {
-              slug
-            }
-            timeToRead
-            frontmatter {
-              date
-              updated_date
-              title
-              start_date
-              end_date
-              is_part_time
-              customer
-              customer_address
-              live_site
-              site_type
-              industry
-              services
-              tools
-              pull_requests
-              cover_image {
-                childImageSharp {
-                  gatsbyImageData(width: 210, height: 118, layout: FIXED)
-                }
-              }
-              image {
-                childImageSharp {
-                  gatsbyImageData(width: 210, height: 118, layout: FIXED)
-                }
-              }
-              lang
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  const posts = get(allMarkdownRemark, 'edges')
+export default function ProjectsIndex({ data }) {
+  const projects = get(data.allMarkdownRemark, 'edges')
 
   return (
     <Content>
@@ -82,7 +42,7 @@ export default function ProjectsIndex() {
       </p>
       <br />
       <UnstyledList>
-        {posts.map(({ node }) => (
+        {projects.map(({ node }) => (
           <ListItem key={node.fields.slug}>
             <ProjectPreview {...{ node }} />
           </ListItem>
@@ -91,3 +51,65 @@ export default function ProjectsIndex() {
     </Content>
   )
 }
+
+export const pageQuery = graphql`
+  query ProjectsPageQuery {
+    last: allMarkdownRemark(
+      filter: { fields: { slug: { regex: "/projects/" } } }
+      sort: { frontmatter: { date: DESC } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            cover_image {
+              childImageSharp {
+                gatsbyImageData(width: 1200, height: 630, layout: FIXED)
+              }
+            }
+          }
+        }
+      }
+    }
+    allMarkdownRemark(
+      filter: { fields: { slug: { regex: "/projects/" } } }
+      sort: { frontmatter: { date: DESC } }
+    ) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          timeToRead
+          frontmatter {
+            date
+            updated_date
+            title
+            start_date
+            end_date
+            is_part_time
+            customer
+            customer_address
+            live_site
+            site_type
+            industry
+            services
+            tools
+            pull_requests
+            cover_image {
+              childImageSharp {
+                gatsbyImageData(width: 210, height: 118, layout: FIXED)
+              }
+            }
+            image {
+              childImageSharp {
+                gatsbyImageData(width: 210, height: 118, layout: FIXED)
+              }
+            }
+            lang
+          }
+        }
+      }
+    }
+  }
+`
