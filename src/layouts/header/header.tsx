75966af437245e8e48368555a252c83a1049c3a5
Sergey Ermakovich <s.ermakovich@gmail.com>
2024-12-19 21:20:53 +0300

    починить меню

--- src/layouts/header/header.tsx
+++ src/layouts/header/header.tsx
@@ -1,14 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { StaticImage } from 'gatsby-plugin-image'
 
 import Content from 'components/content'
 import UnstyledList from 'components/unstyled-list'
 import mq from 'components/media-queries'
-import IconButton from 'components/icon-button'
-
-import HamburgerIcon from './hamburger-icon.inline.svg'
-import CloseIcon from './close-icon.inline.svg'
 
 const menuItemBorderRadius = '.2rem'
 
@@ -25,29 +21,13 @@ const Menu = styled.menu`
   display: flex;
 
   @media (${mq.xs}) {
-    position: fixed;
-    z-index: 1;
-    background: var(--background-color);
-    margin: 0;
-    top: 0;
-    left: 0;
-    right: 0;
-    border-bottom: 0.1rem solid gray;
-    padding: 0 1rem;
-
-    &:not(.open) {
-      display: none;
-    }
+    margin-left: 1rem;
   }
 `
 
 const MenuItems = styled(UnstyledList)`
   flex: none;
   display: flex;
-
-  @media (${mq.xs}) {
-    flex-direction: column;
-  }
 `
 
 const MenuItem = styled.li`
@@ -64,6 +44,11 @@ const MenuLink = styled.a`
   text-decoration: none;
   font-weight: 500;
 
+  @media (${mq.xs}) {
+    padding: 0.3rem;
+    margin: 0.1rem;
+  }
+
   &:active,
   &:hover {
     background-color: rgba(98, 146, 255, 0.2);
@@ -78,40 +63,7 @@ const Main = styled.div`
   flex: 1;
 `
 
-const Right = styled.div`
-  flex: none;
-`
-
-const CloseMenuButton = styled(IconButton)`
-  position: absolute;
-  right: 1rem;
-  top: 1.5rem;
-  display: none;
-
-  @media (${mq.xs}) {
-    display: initial;
-  }
-`
-
-const HamburgerMenuButton = styled(IconButton)`
-  display: none;
-
-  @media (${mq.xs}) {
-    display: initial;
-  }
-`
-
 export default function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-
-  function handleHamburgerIconClick() {
-    setIsMenuOpen(true)
-  }
-
-  function handleCloseIconClick() {
-    setIsMenuOpen(false)
-  }
-
   return (
     <Root>
       <a href="/" title="Главная">
@@ -129,7 +81,7 @@ export default function Header() {
           }}
         />
       </a>
-      <Menu className={isMenuOpen ? 'open' : ''}>
+      <Menu>
         <MenuItems>
           <MenuItem>
             <MenuLink href="/posts/">Записи</MenuLink>
@@ -141,19 +93,8 @@ export default function Header() {
             <MenuLink href="/contacts/">Контакты</MenuLink>
           </MenuItem>
         </MenuItems>
-        <CloseMenuButton aria-label="Close menu" onClick={handleCloseIconClick}>
-          <CloseIcon width="24" height="24" />
-        </CloseMenuButton>
       </Menu>
       <Main />
-      <Right>
-        <HamburgerMenuButton
-          aria-label="Open menu"
-          onClick={handleHamburgerIconClick}
-        >
-          <HamburgerIcon width="24" height="34" />
-        </HamburgerMenuButton>
-      </Right>
     </Root>
   )
 }
